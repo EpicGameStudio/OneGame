d14d465012487d4d9c47b4011db78bc57d2faa76
Roy-H <1246799927@qq.com>
2017-12-25 23:54:11 +0800

    add header and dialog box.

---
+++ OneGame/Assets/Scripts/Helper/DialogTriggerZoneHolder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTriggerZoneHolder : MonoBehaviour {
+
+
+    private DialogManager dialogManager;
+    public string[] dialogLines;
+	// Use this for initialization
+	void Start () {
+        dialogManager = FindObjectOfType<DialogManager>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision == null)
+            return;
+        if (collision.gameObject.name == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (!dialogManager.isDialogBoxActive)
+                {
+                    dialogManager.dialogLines = dialogLines;
+                    dialogManager.currentLine = 0;
+                    dialogManager.SetDialogActive(true);                   
+                }
+                
+            }
+        }
+    }
+}
---
+++ OneGame/Assets/Scripts/Managers/DialogManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour {
+
+
+    #region Fields
+    public GameObject dialogBox;
+    public Text dialogText;
+    public Image dialogHeaderRight;
+    public Image dialogHeaderLeft;
+    public string[] dialogLines;
+    public int currentLine;
+    public bool isDialogBoxActive;
+
+    #endregion
+
+
+    private void Awake()
+    {
+        SetDialogActive(isDialogBoxActive);
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (currentLine >= dialogLines.Length)
+            {
+                SetDialogActive(false);
+                currentLine = 0;
+            }
+            dialogText.text = dialogLines[currentLine];
+            currentLine++;
+        }
+    }
+
+    #region Methods
+    public void SetDialogActive(bool isActive)
+    {
+        if (dialogBox != null)
+        {
+            isDialogBoxActive = isActive;
+            dialogBox.SetActive(isDialogBoxActive);
+        }
+    }
+
+
+    #endregion
+}
