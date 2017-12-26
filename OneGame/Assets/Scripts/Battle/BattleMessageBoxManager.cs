89896d1de478860fc61daaaf547065f351ab1d55
Roy-H <1246799927@qq.com>
2017-12-26 23:31:05 +0800

    add battle scene.

--- OneGame/Assets/Scripts/Battle/BattleMessageBoxManager.cs
+++ OneGame/Assets/Scripts/Battle/BattleMessageBoxManager.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DialogManager : MonoBehaviour {
-
+public class BattleMessageBoxManager : MonoBehaviour {
 
     #region Fields
     public GameObject dialogBox;
     public Text dialogText;
-    public Image dialogHeaderRight;
-    public Image dialogHeaderLeft;
+    //public Image dialogHeaderRight;
+    //public Image dialogHeaderLeft;
     public string[] dialogLines;
     public int currentLine;
     public bool isDialogBoxActive;
+
+    /// <summary>
+    /// when box is not foucsed, pressing doesn't work
+    /// </summary>
+    public bool isFoucs;
 
     #endregion
 
@@ -31,13 +35,15 @@ public class DialogManager : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if (!isFoucs)
+            return;
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            if (dialogLines == null||(dialogLines!=null&& dialogLines.Length<=0))
+            if (dialogLines == null || (dialogLines != null && dialogLines.Length <= 0))
                 return;
             if (currentLine >= dialogLines.Length)
             {
-                SetDialogActive(false);
+                //SetDialogActive(false);
                 currentLine = 0;
             }
             dialogText.text = dialogLines[currentLine];
---
+++ OneGame/Assets/Scripts/Battle/BattleStateMachineManager.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleStateMachineManager : MonoBehaviour
+{
+    //this can define what state should go now and update UI
+
+    Dictionary<string, BattleState> States;
+    public BattleState currentState;
+    private BattleMessageBoxManager dialogManager;
+
+    private void Awake()
+    {
+        dialogManager = FindObjectOfType<BattleMessageBoxManager>();
+        States = new Dictionary<string, BattleState>();
+        InitStates();
+        
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        
+        dialogManager.SetDialogActive(true);
+        GoToState("entry");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    /// <summary>
+    /// all states name:
+    /// 
+    /// 1,entry
+    /// 2,attack
+    /// 3,bag
+    /// 4,team
+    /// 5,runaway
+    /// 
+    /// </summary>
+
+    #region Init States
+    private void InitStates()
+    {
+        if (States == null)
+            States = new Dictionary<string, BattleState>();
+
+
+        List<string> statesList;
+        //1 attack
+        statesList = new List<string> { "attack","bag", "team", "runaway"};
+        States.Add("entry", new BattleState()
+        {
+            Name = "entry",
+            StateAction = () => 
+            {
+                dialogManager.dialogLines = new string[] { "what do you want to do?", "good!!" };
+            },
+            StatesCanGoTo = statesList,
+        });
+
+        statesList = new List<string> { "attack_affect", "entry" };
+        States.Add("attack", new BattleState()
+        {
+            Name = "attack",
+            StateAction = () =>
+            {
+
+            },
+            StatesCanGoTo = statesList,
+        });
+
+        statesList = new List<string> { "bag_affect", "entry" };
+        States.Add("bag", new BattleState()
+        {
+            Name = "bag",
+            StateAction = () =>
+            {
+
+            },
+            StatesCanGoTo = statesList,
+        });
+
+        statesList = new List<string> { "team_affect", "entry" };
+        States.Add("team", new BattleState()
+        {
+            Name = "team",
+            StateAction = () =>
+            {
+
+            },
+            StatesCanGoTo = statesList,
+        });
+
+        statesList = new List<string> { "runaway_affect", "entry" };
+        States.Add("runaway", new BattleState()
+        {
+            Name = "runaway",
+            StateAction = () =>
+            {
+
+            },
+            StatesCanGoTo = statesList,
+        });
+
+    }
+    #endregion
+
+    #region Go to state
+    public void GoToState(string name)
+    {
+        if (States.ContainsKey(name))
+        {
+            currentState = States[name];
+            var temp = currentState.StateAction;
+            if(temp!=null)
+                temp.Invoke();
+        }
+    }
+    #endregion
+}
+
+public class BattleState
+{
+    public string Name { get; set; }
+
+    public Action StateAction { get; set; }
+
+    private List<string> statesCanGoTo = new List<string>();
+
+    public List<string> StatesCanGoTo
+    {
+        get
+        {
+            return statesCanGoTo;
+        }
+        set
+        {
+            statesCanGoTo = value;
+        }
+    }
+}
